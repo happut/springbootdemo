c84a71c7735e10946770bbf7d951b03fb19ce536
bjwangfei3 <bjwangfei3@corp.netease.com>
2017-11-24 10:34:53 +0800

    Initial commit

---
+++ springboot-demo-eureka-server/src/main/java/com/github/happut/springbootdemoeurekaserver/SpringbootDemoEurekaServerApplication.java
@@ -0,0 +1,12 @@
+package com.github.happut.springbootdemoeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemoEurekaServerApplication.class, args);
+	}
+}
