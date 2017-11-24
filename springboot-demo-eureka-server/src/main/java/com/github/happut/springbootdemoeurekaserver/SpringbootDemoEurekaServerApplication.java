9040dd4950393e50c3ec473a1091985b4e58af0e
bjwangfei3 <bjwangfei3@corp.netease.com>
2017-11-24 10:47:57 +0800

    增加enreka 服务

--- springboot-demo-eureka-server/src/main/java/com/github/happut/springbootdemoeurekaserver/SpringbootDemoEurekaServerApplication.java
+++ springboot-demo-eureka-server/src/main/java/com/github/happut/springbootdemoeurekaserver/SpringbootDemoEurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.github.happut.springbootdemoeurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SpringbootDemoEurekaServerApplication {
 
 	public static void main(String[] args) {
