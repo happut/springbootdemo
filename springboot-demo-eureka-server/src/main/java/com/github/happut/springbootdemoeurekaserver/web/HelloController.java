bd4c612df6667ff5503383631e8871b2c6baee06
bjwangfei3 <bjwangfei3@corp.netease.com>
2017-11-24 19:05:34 +0800

    修改配置项

---
+++ springboot-demo-eureka-server/src/main/java/com/github/happut/springbootdemoeurekaserver/web/HelloController.java
@@ -0,0 +1,10 @@
+package com.github.happut.springbootdemoeurekaserver.web;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+
+}
