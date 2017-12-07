e6b81a26d79c2c7270363e28b940f2a905b5a688
bjwangfei3 <bjwangfei3@corp.netease.com>
2017-12-07 18:36:03 +0800

    服务提供者

--- springboot-demo-eureka-client/src/main/java/com/github/happut/springbootdemoeurekaclient/web/HelloController.java
+++ springboot-demo-eureka-client/src/main/java/com/github/happut/springbootdemoeurekaclient/web/HelloController.java
@@ -4,7 +4,6 @@ package com.github.happut.springbootdemoeurekaclient.web;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -14,10 +13,6 @@ import org.springframework.web.client.RestTemplate;
 public class HelloController {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
-
-
-    @Autowired
-    private DiscoveryClient discoveryClient; // 服务发现客户端
 
     @Autowired
     private RestTemplate restTemplate;
