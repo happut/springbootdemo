94757b403f08615e06b0bfa2132a5b4e6c7f0265
bjwangfei3 <bjwangfei3@corp.netease.com>
2017-12-07 18:06:34 +0800

    服务提供者

--- springboot-demo-eureka-provider/src/main/java/com/github/happut/springbootdemoeurekaclient/SpringbootDemoEurekaProviderApplication.java
+++ springboot-demo-eureka-provider/src/main/java/com/github/happut/springbootdemoeurekaclient/SpringbootDemoEurekaProviderApplication.java
@@ -3,17 +3,10 @@ package com.github.happut.springbootdemoeurekaclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class SpringbootDemoEurekaProviderApplication {
-
-    @Bean
-    public RestTemplate createRestTemplate() {
-        return new RestTemplate();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootDemoEurekaProviderApplication.class, args);
